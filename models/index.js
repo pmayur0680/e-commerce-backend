a2a9f362d3b9823b567f0e935c66762cc76a4d6e
Mayur Patel <pmayur0680@gmail.com>
2022-05-04 19:26:19 -0500

    model category, product, rag, producttag, and association defined

--- models/index.js
+++ models/index.js
@@ -4,13 +4,38 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tags_products'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tags, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'products_tags'
+});
 
 module.exports = {
   Product,
